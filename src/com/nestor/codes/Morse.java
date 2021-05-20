af6597a3bf58f5c95c9fe1d9fa1e1ad0cfa6eede
Nestor Eduardo <neim1996@hotmail.com>
2021-05-20 12:40:45 -0600

    Second commit

--- src/com/nestor/codes/Morse.java
+++ src/com/nestor/codes/Morse.java
@@ -3,7 +3,7 @@ package com.nestor.codes;
 public class Morse {
 	public static void main(String[] args) {
 		
-		char[] english = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+		char[] letras = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
 				's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ',', '.',
 				'?' };
 
@@ -19,14 +19,14 @@ public class Morse {
 
 		String str = "";
 		for (int i = 0; i < chars.length; i++) {
-			for (int j = 0; j < english.length; j++) {
+			for (int j = 0; j < letras.length; j++) {
 
-				if (english[j] == chars[i]) {
+				if (letras[j] == chars[i]) {
 					str = str + morse[j] + " ";
 				}
 			}
 		}
-		System.out.println(str);
+		System.out.println("Mensaje encriptado: " + str + "\t(MORSE)");
 
 	}
 }
